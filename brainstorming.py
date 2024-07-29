0262cf818dd7359435fb31fe2be39d7d64b27ba7
devongt <devongracetax@gmail.com>
2024-07-29 16:35:39 -0700

    Created functions for each variable in generate_L1_data

--- brainstorming.py
+++ brainstorming.py
@@ -1,7 +1,7 @@
 
 
 
-def generate_L1_data(CPGS_xml, scene_hdu, proper_arguments, emccd_detect_arguments): 
+def generate_L1_data(CPGS_xml, scene_info, proper_arguments, emccd_detect_arguments): 
     '''
 
     A function that returns a set of L1 data for the given inputs
@@ -28,7 +28,7 @@ def generate_L1_data(CPGS_xml, scene_hdu, proper_arguments, emccd_detect_argumen
 
     Arguments: 
     CPGS_xml: A string that contains the path to the CPGS xml file
-    scene_hdu: An astropy hdu that contains the scene information
+    scene_info: An astropy HDU that contains the scene information
     proper_arguments: A dictionary that contains the proper arguments
     emccd_detect_arguments: A dictionary that contains the emccd detect arguments
 
@@ -37,11 +37,13 @@ def generate_L1_data(CPGS_xml, scene_hdu, proper_arguments, emccd_detect_argumen
     
     '''
 
-    convolved_scene = proper_convolve_2d_scene(scene_hdu, proper_arguments)
+    cpgs_info = open_CPGS_dict(cpgs_xml)
 
-    scene_with_sources = place_point_sources(convolved_scene, scene_hdu)
+    convolved_scene = convolve_2d_scene(scene_info, proper_arguments, cpgs_info)
 
-    scene_with_psf = generate_host_star_psf(scene_with_sources, scene_hdu, proper_arguments)
+    scene_with_sources = place_point_sources(convolved_scene, scene_info, proper_arguments, cpgs_info)
+
+    scene_with_psf = generate_host_star_psf(scene_with_sources, scene_info, proper_arguments)
 
     emccd_scene = simulate_detector(scene_with_psf, emccd_detect_arguments)
 
@@ -50,10 +52,76 @@ def generate_L1_data(CPGS_xml, scene_hdu, proper_arguments, emccd_detect_argumen
     return emccd_scene
 
 
+def open_CPGS_dict(cpgs_xml):
+    '''
+
+    Function that opens a CGPS file, returns a dictionary with relevant info
+
+    '''
+    cpgs_dict = {}
+
+    return cpgs_dict
 
 
+def convolve_2d_scene(scene_info, proper_arguments, cpgs_info):
+    '''
+    
+    Function that convolves a background scene with off-axis PSFs 
+
+    scene_info: a dictionary entry "scene_hdu" that contains an HDU 
+    with a 2D image and a header that contains the pixel scale and center
+
+    proper_arguments and cpgs_info collectively contain rest info we need to generate off-axis
+    PSFs (TBD)
+
+    '''
+    convolved_hdu = None
+    return convolved_hdu
 
 
+def place_point_sources(background_scene, scene_info, proper_arguments, cpgs_info):
+    '''
+    
+    Function that inserts individual off-axis PSFs 
+
+    Arguments:
+    background_scene: a HDU with a 2D image and a header that contains the pixel scale and center
+    scene_info: a dictionary with an entry called "point_source_info" that contains a list of point-source 
+    brightnesses and locations
+    
+    proper_arguments and cpgs_info collectively contain rest info we need to generate off-axis
+    PSFs (TBD) 
+
+    '''
+    point_source_hdu = None
+    return point_source_hdu
 
 
+def generate_host_star_psf(scene_with_sources, scene_info, proper_arguments):
+    '''
 
+    Function that generates a PSF of the host star 
+
+    Arguments: (Max, edit these descriptions as necessary)
+    scene_with_sources: a HDU with a 2D image and a header that contains the placed point sources 
+    scene_info: a dictionary with an entry called "host_star_info" that contains a list of relevant
+    info regarding the host star such as brighteness and location
+    
+    proper_arguments contains rest of info needed to generate off-axis PSFs (TBD)
+    '''
+    host_star_psf_hdu = None
+    return host_star_psf_hdu
+
+
+def simulate_detector(scene_with_psf, emccd_detect_arguments):
+    '''
+    
+    Function that simulates the detector given EMCCD detect arguments and 2D image with PSF
+
+    Arguments: (Max, edit these descriptions as necessary)
+    scene_with_psf: a HDU with a 2D image and a header that contains PSF
+    
+    emccd_detect_arguments contains rest info we need to simulate the detector (TBD)
+    '''
+    simulated_detector_hdu = None
+    return simulated_detector_hdu
