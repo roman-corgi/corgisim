3b51df4ec573e4a8c17f001e603684871f9b9e82
Maxwell A. Millar-Blanchaer <maxmb@ucsb.edu>
2024-11-25 12:06:54 -0800

    generate_observation_sequence function

--- observation.py
+++ observation.py
@@ -1,2 +1,15 @@
 ### Functions that will be run to simulate an observation. 
 ## This will likely contain functions simmilar to the functionality in Jorge's corgisims_obs 
+
+
+def generate_observation_sequence(scene, cgi_optics, cgi_detector, exp_time, n_frames, roll_angles):
+    '''
+    Function that generates a sequence of observations for a given scene, instrument configuration, and detector configuration. 
+
+    - roll angles is a list that needs to match the length of n_frames.
+
+    Arguments: 
+    scene
+    '''
+
+
