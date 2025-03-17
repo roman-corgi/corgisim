2ca8fee01655f8379ce59e204e132068a8f10eeb
zjwhale <jingwen7@hawaii.edu>
2025-03-17 09:20:53 -0700

    resolve the import error

--- corgisim/__init__.py
+++ corgisim/__init__.py
@@ -4,5 +4,5 @@
 __all__ = ['convolution', 'data', 'instrument', 'observation', 'scene',]
 
 __version__ = '0.1'
-__spec__ = __name__
-__package__ = __path__[0]
+#__spec__ = __name__
+#__package__ = __path__[0]
