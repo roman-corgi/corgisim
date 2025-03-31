29ef114e438990454cf55c14bed1853f40f4e883
zjwhale <jingwen7@hawaii.edu>
2025-03-31 14:20:38 -0700

    add pytest test for off_axis_source feature

--- test/test_off_axis_source.py
+++ test/test_off_axis_source.py
@@ -15,8 +15,9 @@ from astropy import units as u
 
 #@pytest.mark.parametrize("interp_method", ['linear', 'cubic'])
 def run_sim():
-    #print('Test if the off axis flat spectrum is correct by comparing with analitical model')
-
+    print('This is a test file to check if the simulated off-axis sources from corgisim agree with those from cgisim')
+    
+    #### simulate using corgisim
     Vmag = 8
     sptype = 'G0V'
     cgi_mode = 'excam'
@@ -27,10 +28,8 @@ def run_sim():
     dx= [3,-3]
     dy= [3,-3]
     
-
     info_dir = cgisim.lib_dir + '/cgisim_info_dir/'
 
-    
     #Define the host star properties
     host_star_properties = {'Vmag': Vmag, 'spectral_type': sptype, 'magtype': 'vegamag'}
     point_source_info = {'Vmag': mag_companion, 'magtype': 'vegamag','position_x':dx , 'position_y':dy}
@@ -47,33 +46,70 @@ def run_sim():
     proper_keywords ={'cor_type':cor_type, 'use_errors':2, 'polaxis':10, 'output_dim':201,\
                     'use_dm1':1, 'dm1_v':dm1, 'use_dm2':1, 'dm2_v':dm2,'use_fpm':1, 'use_lyot_stop':1,  'use_field_stop':1 }
 
-    optics = instrument.CorgiOptics(cgi_mode, bandpass, proper_keywords=proper_keywords, if_quiet=True, integrate_pixels=True)
+    optics = instrument.CorgiOptics(cgi_mode, bandpass, proper_keywords=proper_keywords, if_quiet=True)
     sim_scene = optics.get_psf(base_scene)
-    image = sim_scene.host_star_image.data
+    image_star_corgi = sim_scene.host_star_image.data
 
     sim_scene = optics.inject_point_sources(base_scene,sim_scene)
-    image2 = sim_scene.point_source_image.data 
+    image_comp_corgi = sim_scene.point_source_image.data 
     
-    #print('Final_intensity_get:', np.sum(image, dtype = np.float64))
-    #print(sim_scene.host_star_image[1].header)
+    #### simulate using cgisim
+    polaxis_cgisim = -10
+    params = {'use_errors':1, 'use_dm1':1, 'dm1_v':dm1, 'use_dm2':1, 'dm2_v':dm2}
+    image_star_cgi, a0_counts = cgisim.rcgisim( cgi_mode, cor_type, bandpass,  polaxis_cgisim, params, 
+        star_spectrum=sptype, star_vmag=Vmag )
     
-    fig = plt.figure(figsize=(12,4))
-    plt.subplot(131)
-    plt.imshow(image)
-    plt.title('Host star Vmag=8')
+    image_comp = []
+    for i in range(len(mag_companion )):
+
+        params['source_x_offset']=dx[i]
+        params['source_y_offset']=dy[i]
+        comp_sim_allpol, comp_counts = cgisim.rcgisim( cgi_mode, cor_type, bandpass,  polaxis_cgisim, params, 
+        star_spectrum=sptype, star_vmag=mag_companion[i] )
+
+        image_comp.append(comp_sim_allpol)
+    image_comp_cgi = np.sum(image_comp,axis=0)
+  
+    ####################################Pytest
+    # Use pytest.approx to check similarity within a tolerance
+    assert  image_comp_corgi  == pytest.approx(image_comp_cgi, rel=0.5)
+
+    ####################################make the plots
+    ##if past the test, we will make the plots
+    fig = plt.figure(figsize=(12,8))
+    plt.subplot(231)
+    plt.imshow(image_star_corgi)
+    plt.title('Host star Vmag=8, CorgiSim')
 
     co = plt.colorbar(shrink=0.7)
-    plt.subplot(132)
-    plt.imshow(image2)
-    plt.title('Companion Vmag=25')
+    plt.subplot(232)
+    plt.imshow(image_comp_corgi)
+    plt.title('Companion Vmag=25, CorgiSim')
 
     co = plt.colorbar(shrink=0.7)
 
-    plt.subplot(133)
-    plt.imshow(image2+image)
-    plt.title('Combined Image')
+    plt.subplot(233)
+    plt.imshow(image_star_corgi+image_comp_corgi)
+    plt.title('Combined Image, CorgiSim')
 
     co = plt.colorbar(shrink=0.7)
+
+    plt.subplot(234)
+    plt.imshow(image_star_cgi)
+    plt.title('Host star Vmag=8, CgiSim')
+    co = plt.colorbar(shrink=0.7)
+
+    plt.subplot(235)
+    plt.imshow(image_comp_cgi )
+    plt.title('Companion Vmag=25, CgiSim')
+
+    co = plt.colorbar(shrink=0.7)
+
+    plt.subplot(236)
+    plt.imshow(image_star_corgi+image_comp_corgi )
+    plt.title('Combined Image, CgiSim')
+    co = plt.colorbar(shrink=0.7)
+
     plt.show()
     
 
