4efa64c464f6ab3806c7033871c69432abdce2ae
Sophie Noiret <sophie.noiret@lam.fr>
2025-05-05 09:27:02 +0200

    Modify the test data folder

--- test/test_data_loading.py
+++ test/test_data_loading.py
@@ -8,19 +8,19 @@ def test_cpgs_loading():
         scene_list, optics = data_loader.load_cpgs_data(filepath)  
     assert str(excinfo.value) == filepath +" does not exists." 
     
-    filepath = 'corgisim/data/CGI_0000000000000000014_20221004T2359351_L1_.fits'
+    filepath = 'corgisim/test/test_data/CGI_0000000000000000014_20221004T2359351_L1_.fits'
     with pytest.raises(TypeError) as excinfo:  
         scene_list, optics = data_loader.load_cpgs_data(filepath)  
     assert str(excinfo.value) == filepath +" is not an xml file." 
 
     # Test object creation 
-    filepath = 'corgisim/data/cpgs_without_polarization.xml'
+    filepath = 'corgisim/test/test_data/cpgs_without_polarization.xml'
     scene_list, optics = data_loader.load_cpgs_data(filepath)
     assert isinstance(scene_list[0], scene.Scene)
     assert isinstance(optics, instrument.CorgiOptics)
 
     # Test polarization not implemented
-    filepath = 'corgisim/data/cpgs_default.xml'
+    filepath = 'corgisim/test/test_data/cpgs_default.xml'
     with pytest.raises(NotImplementedError) as excinfo:  
         scene_list, optics = data_loader.load_cpgs_data(filepath)  
     assert str(excinfo.value) == "Only 0/90 deg and 45/135 deg are implemented" 
