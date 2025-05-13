96255f954b18d62c07e98286b6bd5878e04f662e
Sophie Noiret <sophie.noiret@lam.fr>
2025-05-13 08:49:55 +0200

    change data_loader to inputs

--- test/test_inputs.py
+++ test/test_inputs.py
@@ -7,14 +7,14 @@ def test_cpgs_loading():
     # Test error handling 
     filepath = 'wrong/file/path'
     with pytest.raises(FileNotFoundError) as excinfo:  
-        scene_list, optics = data_loader.load_cpgs_data(filepath)  
+        scene_list, optics = inputs.load_cpgs_data(filepath)  
     assert str(excinfo.value) == filepath +" does not exists." 
     
     filepath = 'test_data/cpgs_incorrect_type.txt'
     abs_path =  os.path.join(script_dir, filepath)
 
     with pytest.raises(TypeError) as excinfo:  
-        scene_list, optics = data_loader.load_cpgs_data(abs_path)  
+        scene_list, optics = inputs.load_cpgs_data(abs_path)  
     assert str(excinfo.value) == abs_path +" is not an xml file." 
 
     # Test polarization not implemented
@@ -22,14 +22,14 @@ def test_cpgs_loading():
     abs_path =  os.path.join(script_dir, filepath)
 
     with pytest.raises(NotImplementedError) as excinfo:  
-        scene_list, optics = data_loader.load_cpgs_data(abs_path)  
+        scene_list, optics = inputs.load_cpgs_data(abs_path)  
     assert str(excinfo.value) == "Only 0/90 deg and 45/135 deg are implemented" 
 
     # Test object creation 
     filepath = 'test_data/cpgs_without_polarization.xml'
     abs_path =  os.path.join(script_dir, filepath)
 
-    scene_list, optics = data_loader.load_cpgs_data(abs_path)
+    scene_list, optics = inputs.load_cpgs_data(abs_path)
     assert isinstance(scene_list[0], scene.Scene)
     assert isinstance(optics, instrument.CorgiOptics)
 
