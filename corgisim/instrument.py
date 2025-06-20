7582a66560ea004558e4d7280f75a07ac888c5cc
zjwhale <jingwen7@hawaii.edu>
2025-06-20 11:45:14 -0700

    add functions structure

--- corgisim/instrument.py
+++ corgisim/instrument.py
@@ -48,6 +48,7 @@ class CorgiOptics():
 	- satspot_keywords: A dictionary with the keywords that are used to add satellite spots. See add_satspot for the keywords.
         - oversample: An integer that defines the oversampling factor of the detector when generating the image
         - return_oversample: A boolean that defines whether the function should return the oversampled image or not.
+        - satspot_keywords: A dictionary with the keywords that are used to add satellite spots
     
 
         Raises:
