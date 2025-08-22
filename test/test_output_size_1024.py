fede1b1c7d335e1790dcaeee6277d492a2380be0
Sophie Noiret <sophie.noiret@lam.fr>
2025-08-22 14:24:16 +0200

    fix import

--- test/test_output_size_1024.py
+++ test/test_output_size_1024.py
@@ -11,7 +11,7 @@ from synphot.models import BlackBodyNorm1D, Box1D
 from synphot import units, SourceSpectrum, SpectralElement, Observation
 from synphot.units import validate_wave_unit, convert_flux, VEGAMAG
 from astropy import units as u
-
+import os
 
 @pytest.mark.skipif("LONGTEST" not in os.environ, reason="Long test")
 def test_off_axis_source():
