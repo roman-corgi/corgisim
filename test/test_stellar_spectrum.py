b332e3c8c592dfa6a4aae31cce7212c6f3ad786a
Sophie Noiret <sophienoiret@hotmail.fr>
2026-02-12 15:15:32 -0800

    fix import

--- test/test_stellar_spectrum.py
+++ test/test_stellar_spectrum.py
@@ -7,15 +7,15 @@ import proper
 import roman_preflight_proper
 import pytest
 import cgisim
-from synphot.models import BlackBodyNorm1D, Box1D, Empirical1D
-from synphot import units, SourceSpectrum, SpectralElement, Observation
-from synphot.units import validate_wave_unit, convert_flux, VEGAMAG
+import eetc
+from synphot.models import  Empirical1D
+from synphot import units, SourceSpectrum, SpectralElement
+from synphot.units import VEGAMAG
 from astropy import units as u
 import os
 
 #@pytest.mark.parametrize("interp_method", ['linear', 'cubic'])
 def test_stellar_spectrum():
-    print('Test if the stellar spectrum is correct by comparing with stellar spectum from cgisim')
 
     mag = 5
     sptype_list = ['M5V','M0V','G5V','K0V','G0V','F5V','B3V','A5V','A0V']
@@ -27,9 +27,11 @@ def test_stellar_spectrum():
         spectrum_file = os.path.join(atlas_dir, filename)
 
         data = np.loadtxt(spectrum_file, delimiter=',', comments='#')
-
         wavelength_angstrom = data[:, 0]
         flux = data[:, 1]
+
+        v_band = SpectralElement.from_filter('johnson_v')
+        vega_spec = SourceSpectrum.from_vega()
 
         # Create a synphot SourceSpectrum using Empirical1D model
         sp_eetc = SourceSpectrum(
@@ -43,20 +45,14 @@ def test_stellar_spectrum():
 
     
         #Define the host star properties
-        host_star_properties = {'Vmag': mag, 'spectral_type': sptype[i], 'magtype': 'vegamag'}
+        host_star_properties = {'Vmag': mag, 'spectral_type': sptype, 'magtype': 'vegamag'}
 
         #Create a Scene object that holds all this information
         base_scene = scene.Scene(host_star_properties)
 
         sp=base_scene.stellar_spectrum
         for wave in sp.waveset:
-            assert sp(wave)==sp_eetc_norm(wave), f"Values differ from two methods on sptype {sptype[i]}"
-
-
-    # Use pytest.approx to check similarity within a tolerance
-    #assert cgisim_sp_scale  == pytest.approx(sp2, abs=1e-1), f"Values differ from two methods"
-
-    
+            assert sp(wave)==sp_eetc_norm(wave), f"Values differ from two methods on sptype {sptype}"
 
 if __name__ == '__main__':
     test_stellar_spectrum()
