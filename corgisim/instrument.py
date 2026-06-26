abaccb41ca67d5e2222794a82f2aea9d225afc2c
Sophie Noiret <sophie.noiret@lam.fr>
2026-06-26 14:47:36 +0200

    make wavelength the same for satspot and instrument

--- corgisim/instrument.py
+++ corgisim/instrument.py
@@ -327,6 +327,11 @@ class CorgiOptics():
             # check keywords
             if optics_keywords['use_dm1'] != 1:
                 raise KeyError('ERROR: use_dm1 in optics_keywords is not set 1')
+            if 'wavelength_m' not in satspot_keywords.keys():
+                satspot_keywords['wavelength_m'] =  optics_keywords_internal['lam0']*1e-6
+            elif  satspot_keywords['wavelength_m'] !=  optics_keywords_internal['lam0']*1e-6:
+                warnings.warn('The satellite spot wavelength is different from the optics wavelength ', UserWarning)
+git 
             required_keys_satspot = {'num_pairs','sep_lamD', 'angle_deg', 'contrast', 'wavelength_m'}
             missing_keys = required_keys_satspot - satspot_keywords.keys()
             if missing_keys:
