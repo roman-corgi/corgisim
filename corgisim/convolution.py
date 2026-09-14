6d5705d18752a8307bb0e136ce25d34bc331b5fb
Alexis Lau <40294148+alexisyslau@users.noreply.github.com>
2026-09-14 17:35:18 +0200

    Added the on-axis position back in when setting up the prf grid

--- corgisim/convolution.py
+++ corgisim/convolution.py
@@ -117,9 +117,9 @@ def get_valid_polar_positions(radii_lamD, azimuths_deg):
     """
     azimuths_deg = u.Quantity(azimuths_deg, u.deg)
     radius_grid, azimuth_grid = np.meshgrid(radii_lamD, azimuths_deg, indexing="ij")
-    
-    # Filter out invalid positions: radius=0 with non-zero angle and (0,0)
-    valid_mask = radius_grid > 0.0
+
+    # Have one on-axis PRF at radius=0, angle=0, and exclude all other angles at radius=0, the rest of the grid is valid
+    valid_mask = (radius_grid > 0) | ((radius_grid == 0) & (azimuth_grid == azimuths_deg[0]))    
     valid_radii = radius_grid.ravel()[valid_mask.ravel()]
     valid_azimuths = azimuth_grid.ravel()[valid_mask.ravel()]
     
